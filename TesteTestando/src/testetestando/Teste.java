08eb1447ba236020417133be848ff5780785185a
Uislan Cruz <uislansk8@hotmail.com>
2021-10-26 01:17:30 -0300

    Limpeza

---
+++ TesteTestando/src/testetestando/Teste.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testetestando;
+
+/**
+ *
+ * @author uisla
+ */
+public class Teste {
+public static void main(String[] args) {
+int x, y;
+double z;
+
+x = 5;
+y = 2;
+z = x / y;
+System.out.print(z);
+}
+}
+
+    
+
