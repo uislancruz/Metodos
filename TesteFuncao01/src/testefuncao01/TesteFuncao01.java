962f75992dfea07d87794246d4863f4fd55f09ac
Uislan Cruz <uislansk8@hotmail.com>
2021-10-25 22:53:03 -0300

    Static

--- TesteFuncao01/src/testefuncao01/TesteFuncao01.java
+++ TesteFuncao01/src/testefuncao01/TesteFuncao01.java
@@ -10,12 +10,19 @@ package testefuncao01;
  * @author uisla
  */
 public class TesteFuncao01 {
+    static int soma (int a, int b){
+        int s = a + b;
+        return s;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Começou o programa");
+         int sm = soma(5,2); 
+         System.out.println("A soma é: " +sm);
     }
     
 }
